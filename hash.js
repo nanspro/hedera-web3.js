dce4ab9a83bf089ae916953adcdcda0ac312ce83
Sounak Pradhan <sounak.98@gmail.com>
2019-12-18 00:49:53 +0530

    Update

--- hash.js
+++ hash.js
@@ -3,6 +3,18 @@ let hash = {};
 window.onload = function() {
   window.hash = hash;
 
+  const returnResponse = (xhr) => {
+    return new Promise((resolve, reject) => {
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+          resolve(xhr.response);
+        }
+      };
+
+      xhr.send();
+    });
+  }
+
   const getUrl = (url, params = null) => {
     if (params === null) {
       return `https://cors-anywhere.herokuapp.com/https://api.dragonglass.me/hedera/api/${url}`;
@@ -16,16 +28,7 @@ window.onload = function() {
 
     xhr.open("GET", getUrl("accounts"), true);
     xhr.setRequestHeader("X-API-KEY", "974d4207-51e0-3d73-8e20-4d5952078c47");
-
-    return new Promise((resolve, reject) => {
-      xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-          resolve(xhr.response);
-        }
-      };
-
-      xhr.send();
-    });
+    returnResponse(xhr);
   };
 
   hash.accountBalanceAsOf = data => {};
