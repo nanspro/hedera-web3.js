d70c43b832f0b936e3f552c427367add182624ec
Sounak Pradhan <sounak.98@gmail.com>
2019-12-18 00:54:55 +0530

    Update

--- hash.js
+++ hash.js
@@ -2,8 +2,15 @@ let hash = {};
 
 window.onload = function() {
   window.hash = hash;
+  var accessKey = "";
 
-  const getResponsePromise = (xhr) => {
+  const setAccessKey = (
+    _accessKey = "974d4207-51e0-3d73-8e20-4d5952078c47"
+  ) => {
+    accessKey = _accessKey;
+  };
+
+  const getResponsePromise = xhr => {
     return new Promise((resolve, reject) => {
       xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
@@ -13,7 +20,7 @@ window.onload = function() {
 
       xhr.send();
     });
-  }
+  };
 
   const getUrl = (url, params = null) => {
     if (params === null) {
@@ -27,7 +34,7 @@ window.onload = function() {
     const xhr = new this.XMLHttpRequest();
 
     xhr.open("GET", getUrl("accounts"), true);
-    xhr.setRequestHeader("X-API-KEY", "974d4207-51e0-3d73-8e20-4d5952078c47");
+    xhr.setRequestHeader("X-API-KEY", accessKey);
     return getResponsePromise(xhr);
   };
 
